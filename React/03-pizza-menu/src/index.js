67184c857249ce3fa6a416ca6bb40189aee128fe
tanjim <tanjim13hasan@gmail.com>
2024-01-03 21:25:30 +0600

    49-Conditional-Rendering-With-Multiple-Returns

--- React/03-pizza-menu/src/index.js
+++ React/03-pizza-menu/src/index.js
@@ -106,6 +106,8 @@ function Menu() {
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
