8c54e397ef432662ea399dc6ee64eb6945bb1e95
tanjim <tanjim13hasan@gmail.com>
2024-01-03 19:49:55 +0600

    46-Rendering-Lists

--- React/03-pizza-menu/src/index.js
+++ React/03-pizza-menu/src/index.js
@@ -73,7 +73,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Ore menu</h2>
-      <Pizza
+
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+
+      {/* <Pizza
         name="Pizza Spinaci"
         ingredients="Tomato, mozarella, spinach and ricotta cheese"
         photoName="pizzas/spinaci.jpg"
@@ -85,21 +92,21 @@ function Menu() {
         ingredients="Tomato, mushroom"
         photoName="pizzas/funghi.jpg"
         price="$7"
-      />
+      /> */}
     </main>
   );
 }
 
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
