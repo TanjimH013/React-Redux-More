f7f079c7f3c3684c77ed2b85e86ddc4f790c01f1
tanjim <tanjim13hasan@gmail.com>
2024-02-02 11:32:02 +0600

    119-Improving-Reusability-With-Props

--- React/07-use-pop-corn/src/index.js
+++ React/07-use-pop-corn/src/index.js
@@ -8,7 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="#DC143C" className="test" />
   </React.StrictMode>
 );
 
