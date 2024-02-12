f0e02aa9e0ee6ba0e2e5497b560855033ae5e2fd
tanjim <tanjim13hasan@gmail.com>
2024-02-12 10:21:37 +0600

    143-useEffect-to-the-Rescue-Add-Dependency-Array

--- React/07-use-pop-corn/src/App.js
+++ React/07-use-pop-corn/src/App.js
@@ -60,7 +60,7 @@ export default function App() {
     fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
       .then((res) => res.json())
       .then((data) => setMovies(data.Search));
-  });
+  }, []);
 
   return (
     <>
