9374ae6213805fa624f64038c91f783ccb797fae
tanjim <tanjim13hasan@gmail.com>
2024-03-06 10:41:12 +0600

    Create-FormikApp-Project

---
+++ React-Anisul-Islam/Project/Form-Validation/form/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FormikApp from "./components/FormikApp";
+
+function App() {
+  return (
+    <div className="App">
+      <FormikApp />
+    </div>
+  );
+}
+
+export default App;
