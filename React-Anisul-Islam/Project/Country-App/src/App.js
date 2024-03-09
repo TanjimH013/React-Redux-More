317937b51cb34a0bc6ac4b5e46a4caebabae1d16
tanjim <tanjim13hasan@gmail.com>
2024-03-09 19:40:07 +0600

    Add-Search-in-CountryApp

--- React-Anisul-Islam/Project/Country-App/src/App.js
+++ React-Anisul-Islam/Project/Country-App/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import Countries from "./components/Countries";
 
 import "./App.css";
+import Search from "./components/Search";
 
 const url = "https://restcountries.com/v3.1/all";
 
@@ -39,9 +40,19 @@ const App = () => {
     setFilteredCountries(filter);
   };
 
+  const handleSearch = (searchValue) => {
+    let value = searchValue.toLowerCase();
+    const newCountries = countries.filter((country) => {
+      const countryName = country.name.common.toLowerCase();
+      return countryName.startsWith(value);
+    });
+    setFilteredCountries(newCountries);
+  };
+
   return (
     <>
       <h1>🌎 🌏 🌍 Country App 🌎 🌏 🌍</h1>
+      <Search onSearch={handleSearch} />
       {isLoading && <h2>Loading.... 🥱😴</h2>}
       {error && <h2>{error.message}</h2>}
       {countries && (
