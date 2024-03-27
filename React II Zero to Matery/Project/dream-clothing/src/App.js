3644b6d912bf21d82a9ecf43aa3bf9ba82fc94cf
tanjim <tanjim13hasan@gmail.com>
2024-03-27 14:43:20 +0600

    Add-SASS-File-to-Dream-clothing-Project

--- React II Zero to Matery/Project/dream-clothing/src/App.js
+++ React II Zero to Matery/Project/dream-clothing/src/App.js
@@ -1,37 +1,24 @@
 import React from "react";
 
+import "./categories.style.scss";
+
 const App = () => {
   const categories = [
-    {
-      id: 1,
-      title: "Hate",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-    },
-    {
-      id: 4,
-      title: "Womens",
-    },
-    {
-      id: 5,
-      title: "Mens",
-    },
+    { id: 1, title: "Hates" },
+    { id: 2, title: "Jackets" },
+    { id: 3, title: "Sneakers" },
+    { id: 3, title: "Mens" },
+    { id: 1, title: "Womens" },
   ];
 
   return (
     <div className="categories-container">
       {categories.map(({ title }) => (
         <div className="category-container">
-          <div className="background-image" />
+          <div className="background-image" img />
           <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
+            <h1>{title}</h1>
+            <p>Shope Now</p>
           </div>
         </div>
       ))}
