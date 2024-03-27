afb1e3a32e164a1a7457472823c17551a9eea2c6
tanjim <tanjim13hasan@gmail.com>
2024-03-27 16:27:17 +0600

    Add-ImagUrl-in-categories-object

--- React II Zero to Matery/Project/dream-clothing/src/App.js
+++ React II Zero to Matery/Project/dream-clothing/src/App.js
@@ -4,20 +4,31 @@ import "./categories.style.scss";
 
 const App = () => {
   const categories = [
-    { id: 1, title: "Hates" },
-    { id: 2, title: "Jackets" },
-    { id: 3, title: "Sneakers" },
-    { id: 4, title: "Mens" },
-    { id: 5, title: "Womens" },
+    { id: 1, title: "Hates", imageUrl: "https://i.ibb.co/cvpntL1/hats.png" },
+    {
+      id: 2,
+      title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/Jackets.png",
+    },
+    {
+      id: 3,
+      title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    { id: 4, title: "Mens", imageUrl: "https://i.ibb.co/R70vBrQ/men.png" },
+    { id: 5, title: "Womens", imageUrl: "https://i.ibb.co/GCCdy8t/womens.png" },
   ];
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => (
+      {categories.map(({ title, id, imageUrl }) => (
         <div key={id} className="category-container">
-          <div className="background-image" img />
+          <div
+            className="background-image"
+            style={{ backgroundImage: `url(${imageUrl})` }}
+          />
           <div className="category-body-container">
-            <h2>{title}</h2>
+            <h1>{title}</h1>
             <p>Shope Now</p>
           </div>
         </div>
