6d9fbc67e835bae140707331359a124685fb583d
tanjim <tanjim13hasan@gmail.com>
2024-03-28 15:56:55 +0600

    Setting-Up-Our-Homepage-and-use-Routes-Route-to-Dream-Clothing

--- React II Zero to Matery/Project/dream-clothing/src/App.js
+++ React II Zero to Matery/Project/dream-clothing/src/App.js
@@ -1,27 +1,12 @@
-import React from "react";
-import Directory from "./components/directory/directory";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./routes/home.component";
 
 const App = () => {
-  const categories = [
-    { id: 1, title: "Hates", imageUrl: "https://i.ibb.co/cvpntL1/hats.png" },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/Jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    { id: 4, title: "Mens", imageUrl: "https://i.ibb.co/R70vBrQ/men.png" },
-    { id: 5, title: "Womens", imageUrl: "https://i.ibb.co/GCCdy8t/womens.png" },
-  ];
-
   return (
-    <div>
-      <Directory categories={categories} />
-    </div>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+    </Routes>
   );
 };
 
