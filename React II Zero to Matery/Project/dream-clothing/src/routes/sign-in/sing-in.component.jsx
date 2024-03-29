a392243cad7f1e972eb8b5a7c8b899e9cd3e6c10
tanjim <tanjim13hasan@gmail.com>
2024-03-29 20:32:31 +0600

    Authenticating-With-Firebase-Setting-Setup-To-Dream-Clothing

--- React II Zero to Matery/Project/dream-clothing/src/routes/sign-in/sing-in.component.jsx
+++ React II Zero to Matery/Project/dream-clothing/src/routes/sign-in/sing-in.component.jsx
@@ -1,7 +1,14 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
   return (
     <>
       <h1>I am Sign In</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
     </>
   );
 };
--- React II Zero to Matery/Project/dream-clothing/src/utils/firebase/firebase.utils.js
+++ React II Zero to Matery/Project/dream-clothing/src/utils/firebase/firebase.utils.js
@@ -1,3 +1,29 @@
-const FirebaseUtils = () => {};
+import { initializeApp } from "firebase/app";
 
-export default FirebaseUtils;
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjWKudP8a12NaadhQJu5mutvtkLHjf1Io",
+  authDomain: "dream-clothing-db-f2ba5.firebaseapp.com",
+  projectId: "dream-clothing-db-f2ba5",
+  storageBucket: "dream-clothing-db-f2ba5.appspot.com",
+  messagingSenderId: "205974619100",
+  appId: "1:205974619100:web:72eb8992c11606fb280519",
+};
+
+// Initialize Firebase
+const firebaseAPP = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
