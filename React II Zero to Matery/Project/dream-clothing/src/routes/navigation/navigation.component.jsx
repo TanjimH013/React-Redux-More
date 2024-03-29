53dc1a437247ede7b87d349c1e1e164ef158b07b
tanjim <tanjim13hasan@gmail.com>
2024-03-29 12:14:39 +0600

    Styling-for-Navigation-plus-Logo-to-Dream-Clothing-Project

--- React II Zero to Matery/Project/dream-clothing/src/routes/navigation/navigation.component.jsx
+++ React II Zero to Matery/Project/dream-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,15 @@
 import { Outlet, Link } from "react-router-dom";
 
+import { ReactComponent as DrmLogo } from "../../assets/logo.svg";
+
+import "./navigation.styles.scss";
+
 const Navigation = () => {
   return (
     <>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>LOgo</div>
+          <DrmLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/Shop">
