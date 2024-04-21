d5a6d23abb8fd567319dd212c849b206c5ffa8ef
tanjim <tanjim13hasan@gmail.com>
2024-04-21 19:04:41 +0600

    Create checkout page along with checkout items and checkout cart functionality

--- React II Zero to Matery/Project/dream-clothing/src/components/checkout/checkout.component.jsx
+++ React II Zero to Matery/Project/dream-clothing/src/components/checkout/checkout.component.jsx
@@ -2,29 +2,38 @@ import { useContext } from "react";
 
 import { CartContext } from "../../context/cart.context";
 
+import CheckoutItem from "./checkout-item.component";
+
 import "./checkout.style.scss";
 
 const Checkout = () => {
-  const { cartItems, addItemToCart } = useContext(CartContext);
+  const { cartItems, cartTotal } = useContext(CartContext);
 
   return (
-    <div>
-      <h1>I am the CheckOut page</h1>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <span>{quantity}</span>
-              <br />
-              <span>➖</span>
-              <br />
-              <span onClick={() => addItemToCart(cartItem)}>➕</span>
-            </div>
-          );
-        })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>Product</span>
+        </div>
+
+        <div className="header-block">
+          <span>Description</span>
+        </div>
+
+        <div className="header-block">
+          <span>Price</span>
+        </div>
+
+        <div className="header-block">
+          <span>Remove</span>
+        </div>
+
+        <div className="header-block"></div>
       </div>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <span className="Total">Total: {cartTotal}</span>
     </div>
   );
 };
